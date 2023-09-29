c2e7e0da282e44ab4df394b15f419c7ae73d1bc5
diyamore19 <146319887+diyamore19@users.noreply.github.com>
2023-09-29 13:50:40 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,13 @@
+// data.js
+const profiles = [
+  {
+    id: 1,
+    name: "Jay Soni",
+    address: "123 Main St, City1, Country1",
+    latitude: 40.7128,
+    longitude: -74.0060,
+  },
+  // Add more profiles here
+];
+
+export default profiles;
